3be67f531660da56e5d49a0a59ce2ede3b80c57a
SwiftkeyX <pisanucaraleadkul.pasit@gmail.com>
2026-06-14 10:05:32 +0700

    Revert "Merge pull request #5 from SwiftkeyX/refactor/restore-phase2"
    This reverts commit f2492a7b0860e7a74834494cd18dad2d4cc75865, reversing
    changes made to 37f527cc0966004310b5c8a4d23aec222cebdd29.

--- Assets/Scripts/IDamageable.cs
+++
@@ -1,6 +0,0 @@
-/// <summary>Implemented by any entity that can be hit by a projectile. Decouples Projectile from concrete target types.</summary>
-public interface IDamageable
-{
-    Team Team { get; }
-    void TakeDamage(int damage);
-}
